8abe9825c134e37a2d84dd651c98e1c0a4a9b934
Edward <Edwardtonnn@gmail.com>
2024-01-16 09:31:44 -0600

    adds

---
+++ contact-us.php
@@ -0,0 +1,248 @@
+<section class="header animate fadeIn no-delay has-mega-matrix img-matrix">
+  <nav class="navbar container" role="navigation" aria-label="main navigation">
+    <div class="navbar-links">
+      <div class="navbar-link-container is-hidden-widescreen">
+        <div class="flexy flexd-clmn">
+        <a onclick="gtag('event', 'phone_number_click');" aria-label="Call <?php echo $sitename; ?>" href="tel:<?php echo $phone; ?>">
+          <p class="navbar-link mr0 mb0">DUMBO // Brooklyn, NY </p>
+        </a>
+        <a class="navbar-link navbar-phone moss" onclick="gtag('event', 'phone_number_click');" aria-label="Call <?php echo $sitename; ?>" href="tel:<?php echo $phone; ?>">  <?php echo $phone ?></a>
+
+        </div>
+      </div>
+      
+
+      <a class="navbar-brand" href="/">
+        <img class="navbar__logo" src="/assets/img/logo.svg" alt="<?php echo $sitename; ?> Logo" />
+      </a>
+
+      <div class="flexy">
+        <div class="flexy flexd-clmn">
+          <a class="white navbar-link" href="/about/contact/">Request Consultation<img class="arrow-right white" src="/assets/img/_defaults/arrow-right-accent.svg" alt="right arrow" /></a>
+
+          <a class="white navbar-link" href="https://book.mypatientnow.com/practice/lbSyLz" rel="noopener" target="_blank" onclick="gtag('event', 'booking_platform_click');" aria-label="Book Appointment at <?php echo $sitename; ?> (Opens a new tab)">Schedule Online<img class="arrow-right white" src="/assets/img/_defaults/arrow-right-accent.svg" alt="right arrow" /></a>
+          
+          <a class="white navbar-link" href="https://www.renatafacemdskin.com/" target="_blank" rel="noopener">Shop Skincare<img class="arrow-right white" src="/assets/img/_defaults/arrow-right-accent.svg" alt="right arrow" /></a>
+
+        </div>
+        <div class="menu-button" aria-label="Menu Button">
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <label>Menu</label>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div class="container text-center">
+    <a href="/about/contact/">
+      <p class="banner-text caption uppercase animate fadeIn mw800">Contact us today</p>
+    </a>
+  </div>
+</div>
+  <menu class="mega-menu">
+    <div class="container">
+      <!-- MOBILE MENU  -->
+    <div class="top-menu menu-selectors desktop-hidden pv50 bg-white ps20">
+					<div class="accordion__alt animate fadeIn">
+            <div class="accordion__item">
+              <div class="accordion__question">
+                <i></i>
+                <h3 class="lead">Facial Surgery</h3>
+              </div>
+              <div class="accordion__answer" style="max-height: 0px;">
+              <div class="flexy flexd-clmn">
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/facelift/">Facelift</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/deep-plane-facelift/">Deep Plane Facelift</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/mini-facelift/">Mini Facelift</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/neck-lift/">Neck Lift</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/neck-liposuction/">Neck Liposuction</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/blepharoplasty/">Blepharoplasty (Eyelid Lift)</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/brow-lift/">Brow Lift</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/lip-lift/">Lip Lift</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/lip-lift/">Rhinoplasty (Nasal Reshaping)</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/buccal-fat-removal/">Buccal Fat Removal</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/otoplasty/">Otoplasty (Ear Shaping)</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/earlobe-surgery/">Earlobe Surgery</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/surgical-scar-revision/">Surgical Scar Revision</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/facial-lesion-removal/">Facial Lesion Removal</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/chin-enhancement/">Chin Enhancement</a>
+                <a class="highlight-color lead nav-link" href="/cosmetic-surgery/facial-implant/">Facial Implants</a>
+              </div>
+              </div>
+            </div>
+            <div class="accordion__item">
+              <div class="accordion__question">
+                <i></i>
+                <h3 class="lead">Non-Surgical</h3>
+              </div>
+              <div class="accordion__answer" style="max-height: 0px;">
+              <div class="flexy flexd-clmn">
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/facelift/">Facelift</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/deep-plane-facelift/">Deep Plane Facelift</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/mini-facelift/">Mini Facelift</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/neck-lift/">Neck Lift</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/neck-liposuction/">Neck Liposuction</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/blepharoplasty/">Blepharoplasty (Eyelid Lift)</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/brow-lift/">Brow Lift</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/lip-lift/">Lip Lift</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/lip-lift/">Rhinoplasty (Nasal Reshaping)</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/buccal-fat-removal/">Buccal Fat Removal</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/otoplasty/">Otoplasty (Ear Shaping)</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/earlobe-surgery/">Earlobe Surgery</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/surgical-scar-revision/">Surgical Scar Revision</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/facial-lesion-removal/">Facial Lesion Removal</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/chin-enhancement/">Chin Enhancement</a>
+              <a class="highlight-color lead nav-link" href="/cosmetic-surgery/facial-implant/">Facial Implants</a>
+              </div>
+              </div>
+            </div>
+            <div class="accordion__item">
+              <div class="accordion__question">
+                <i></i>
+                <h3 class="lead">Skin Enhancement</h3>
+              </div>
+              <div class="accordion__answer" style="max-height: 0px;">
+              <div class="flexy flexd-clmn">
+              <a class="highlight-color lead nav-link" href="/skin-treatments/microneedling/">Microneedling</a>
+              <a class="highlight-color lead nav-link" href="/injectables-fillers/prp-injections/">PRP Injection</a>
+              <a class="highlight-color lead nav-link" href="/skin-treatments/skincare/">Professional Skincare</a>
+              </div>
+              </div>
+            </div>
+            <div class="accordion__item">
+              <div class="accordion__question">
+                <i></i>
+                <h3 class="lead">Laser & Energy</h3>
+              </div>
+              <div class="accordion__answer" style="max-height: 0px;">
+              <div class="flexy flexd-clmn">
+              <a class="highlight-color lead nav-link" href="/laser-treatments/co2-laser-resurfacing/">CO2 Laser Skin Resurfacing</a>
+              </div>
+              </div>
+            </div>
+            <div class="accordion__item">
+              <div class="accordion__question">
+                <i></i>
+                <h3 class="lead">Patient Resources</h3>
+              </div>
+              <div class="accordion__answer" style="max-height: 0px;">
+              <div class="flexy flexd-clmn">
+              <a class="highlight-color lead nav-link" href="/about/dr-khelemsky/">Meet Dr. Renata</a>
+                <a class="highlight-color lead nav-link" href="/about/philosophy/">Practice Philosophy</a>
+                <a class="highlight-color lead nav-link" href="/about/our-clinic/">Our Clinic</a>
+                <a class="highlight-color lead nav-link" href="/blog/">Blog</a>
+                <a class="highlight-color lead nav-link" href="/about/testimonials/">Testimonials</a>
+                <a class="highlight-color lead nav-link" href="/financing/">Financing</a>
+              </div>
+              </div>
+            </div>
+          </div>
+					<div class="flexy gap50 is-vcentered pv20 ps20 mobile-newsletter">
+        <div class="flexy-item">
+          <p class="animate fadeIn title-sm text-center">Join the DR. RENATA FACE OF BEAUTY Club</p>
+        </div>
+        <div class="flexy-item">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/assets/inc/form-newsletter.php" ?>
+        </div>
+      </div>
+					
+				</div>
+    </div>
+    
+    <div class="main-menu-items mobile-hidden"> 
+      <!-- DESKTOP MENU       -->
+    <div class="main-menu-bg pt20 pb50 ps20">
+      <div class="flexy gap20">
+        <div class="flexy-item flex-column">
+          <p class="lead mb10">Facial Surgery</p>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/facelift/">Facelift</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/deep-plane-facelift/">Deep Plane Facelift</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/mini-facelift/">Mini Facelift</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/neck-lift/">Neck Lift</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/neck-liposuction/">Neck Liposuction</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/blepharoplasty/">Blepharoplasty (Eyelid Lift)</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/brow-lift/">Brow Lift</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/lip-lift/">Lip Lift</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/lip-lift/">Rhinoplasty (Nasal Reshaping)</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/buccal-fat-removal/">Buccal Fat Removal</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/otoplasty/">Otoplasty (Ear Shaping)</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/earlobe-surgery/">Earlobe Surgery</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/surgical-scar-revision/">Surgical Scar Revision</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/facial-lesion-removal/">Facial Lesion Removal</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/chin-enhancement/">Chin Enhancement</a>
+          <a class="highlight-color lead nav-link" href="/cosmetic-surgery/facial-implant/">Facial Implants</a>
+        </div>
+        <div class="flexy-item flex-column">
+        <p class="lead mb10">Non-Surgical</p>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/dermal-fillers/">Dermal Fillers</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/juvederm-voluma-xc/">Juvederm Voluma</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/liquid-facelift/">The FaceFrame™ Lift</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/lip-fillers/">Lip Fillers</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/botox/">Botox</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/xeomin/">Xeomin</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/dysport/">Dysport</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/kybella/">Kybella</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/non-surgical-rhinoplasty/">Liquid Rhinoplasty</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/tmj-botox/">TMJ Botox</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/non-surgical-brow-lift/">Nonsurgical Brow Lift</a>
+        </div>
+        <div class="flexy-item flex-column">
+        <p class="lead mb10">Skin Enhancement</p>
+        <a class="highlight-color lead nav-link" href="/skin-treatments/microneedling/">Microneedling</a>
+        <a class="highlight-color lead nav-link" href="/injectables-fillers/prp-injections/">PRP Injection</a>
+        <a class="highlight-color lead nav-link" href="/skin-treatments/skincare/">Professional Skincare</a>
+        </div>
+        <div class="flexy-item flex-column">
+        <p class="lead mb10">Laser & Energy</p>
+        <a class="highlight-color lead nav-link" href="/laser-treatments/co2-laser-resurfacing/">CO2 Laser Skin Resurfacing</a>
+        </div>
+        <div class="flexy-item flex-column">
+        <p class="lead mb10">Patient Resources</p>
+        <a class="highlight-color lead nav-link" href="/about/dr-khelemsky/">Meet Dr. Renata</a>
+        <a class="highlight-color lead nav-link" href="/about/philosophy/">Practice Philosophy</a>
+        <a class="highlight-color lead nav-link" href="/about/our-clinic/">Our Clinic</a>
+        <a class="highlight-color lead nav-link" href="/blog/">Blog</a>
+        <a class="highlight-color lead nav-link" href="/about/testimonials/">Testimonials</a>
+        <a class="highlight-color lead nav-link" href="/financing/">Financing</a>
+        </div>
+      </div>
+    </div>
+    <div class="flexy gap50 is-vcentered pv20 ps20">
+        <div class="flexy-item">
+          <p class="animate fadeIn title-sm">Join the DR. RENATA FACE OF BEAUTY Club</p>
+        </div>
+        <div class="flexy-item">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/assets/inc/form-newsletter.php" ?>
+        </div>
+      </div>
+
+    <div class="is-visible-desktop flexy">
+      <a class="menuAda mt15 mb0" href="/about/contact/">Request Consultation</a>
+      <a class="menuAda mb0" href="https://book.mypatientnow.com/practice/lbSyLz" rel="noopener" target="_blank">Schedule Online</a>
+      <a class="menuAda mb0" href="https://www.renatafacemdskin.com/" target="_blank" rel="noopener">Shop Skincare</a>
+      <a id="adaWidget" class="menuAda" aria-label="Open accessibility menu">Accessibility <img src="https://cdn.userway.org/widgetapp/images/body_wh.svg" alt="Accessibility Widget" /></a>
+      <div class="nav-socials">
+      <?php include $_SERVER['DOCUMENT_ROOT'] . "/assets/inc/social.php" ?>
+      </div>
+    </div>
+  </menu>
+</section>
+
+<div class="mobile-cta">
+  <a href="tel:<?php echo $phone; ?>" onclick="gtag('event', 'phone_number_click');" aria-label="Call <?php echo $sitename; ?>"><?php echo $phone ?></a>
+  <a href="/about/contact/">Appointment</a>
+</div>
+
+<div class="overlay">
+</div>
+
+<div class="modal">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+  </div>
+  <button class="modal-close is-large" aria-label="close"></button>
+</div>
+
--- init-repo/script.js
+++ init-repo/script.js
@@ -1,12 +1,12 @@
 const menu = document.querySelector('.menu');
 const lines = menu.querySelectorAll('span.line');
 
-menu.addEventListener('mouseenter', () => {
+menu.addEventListener('click', () => {
   lines[0].classList.add('rotated45');
   lines[1].classList.add('rotatedMinus45');
 });
 
-menu.addEventListener('mouseleave', () => {
+menu.addEventListener('click', () => {
   lines[0].classList.remove('rotated45');
   lines[1].classList.remove('rotatedMinus45');
 });
