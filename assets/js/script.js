ccf83bbaf15d1c5cba5ff168f9a2668a844a1223
Edward <Edwardtonnn@gmail.com>
2024-01-22 08:53:30 -0600

    update menu - now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,66 @@
-// const gsap = require('C:/Sandbox/node_modules/gsap/dist/gsap');
+// GSAP Installed 
 
-// import { gsap } from "C:/Sandbox/node_modules/gsap/dist/gsap";
-
-import { gsap } from "gsap";
-
-
-
-
-gsap.to(".box", { rotation: 27, x: 100, duration: 1 });
+// Menu functionality 
+// On click of menu transform to x and on click again transform back to hamburger see if the is a way to toggle it. 
 
 const menu = document.querySelector('.menu');
 const lines = menu.querySelectorAll('span.line');
+const menuText = document.querySelectorAll('.logo, .contact a');
+const menuContainer = document.querySelector('.menu-container');
 
-menu.addEventListener('mouseenter', () => {
-  lines[0].classList.add('rotated45');
-  lines[1].classList.add('rotatedMinus45');
+menu.addEventListener('click', () => {
+
+  // Checks if there is a closed class if it is closed it will remove the class so the lines can opener. 
+  if (menu.classList.contains('open')) {
+    lines[0].classList.remove('rotated45');
+    lines[1].classList.remove('rotatedMinus45');
+
+    // close the menu here 
+    closeMenu();
+
+    menuText.forEach(menuTextItem =>{
+      menuTextItem.classList.remove('white');
+    });
+
+  } else {
+    lines[0].classList.add('rotated45');
+    lines[1].classList.add('rotatedMinus45');
+
+    // open the menu here
+    openMenu();  
+    menuText.forEach(menuTextItem =>{
+      menuTextItem.classList.add('white');
+    });  
+  }
+
+  menu.classList.toggle('open');
+
+   // close menu function 
+  function closeMenu(){
+    // gsap.to('.menu-container', {display:'none'})
+    menuContainer.style.display = 'none'
+
+    revertColors();
+  }
+  
+  // open menu function 
+  function openMenu(){
+    gsap.to('.menu-container', {display:'block'})
+
+    lines.forEach(line => {
+      line.style.borderBottom = '0.5px solid white';
+    });
+  }
+
+  function revertColors(){
+    lines.forEach(line => {
+      line.style.borderBottom = '0.5px solid #1a1a1a';
+    });
+
+  
+  
+  }
 });
 
-menu.addEventListener('mouseleave', () => {
-  lines[0].classList.remove('rotated45');
-  lines[1].classList.remove('rotatedMinus45');
-});
+
+
